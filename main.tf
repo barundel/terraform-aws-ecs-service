7a1dbc9c8c45cec5fbab44ced86482ace75024c9
Ben Arundel <ben.arundel@cdl.co.uk>
2019-09-16 15:25:12 +0100

    adding in log group creation

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ variable "service_name" {}
 variable "desired_count" {default = 1}
 variable "iam_role" {default = ""}
 variable "container_definitions" {}
+variable "create_log_group" {default = false}
+variable "log_group_name" {}
+variable "tags" {type = "map" default = {}}
+variable "log_group_retention" {default = 7}
 
 resource "aws_ecs_service" "service" {
   name            = var.service_name
@@ -30,4 +34,13 @@ resource "aws_ecs_task_definition" "task" {
     type       = "memberOf"
     expression = "attribute:ecs.availability-zone in [eu-west-1a, eu-west-1b, eu-west-1c]"
   }
+}
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  count = "${var.create_log_group ? 1 : 0}"
+
+  name              = var.log_group_name
+  retention_in_days = var.log_group_retention
+
+  tags = var.tags
 }
